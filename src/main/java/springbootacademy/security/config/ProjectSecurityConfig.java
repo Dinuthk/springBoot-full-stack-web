0b3d23969bcd8f39c1bcd3ca537ec006f4f49b77
Dinuth Diruksha <dinuthk69@gmail.com>
2025-02-01 11:10:08 +0530

    backend

--- src/main/java/springbootacademy/security/config/ProjectSecurityConfig.java
+++ src/main/java/springbootacademy/security/config/ProjectSecurityConfig.java
@@ -2,6 +2,7 @@ package springbootacademy.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -11,6 +12,10 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableGlobalMethodSecurity(
+        prePostEnabled = true,
+        securedEnabled = true
+)
 public class ProjectSecurityConfig {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception{ //csrf().disable() meka dnw spring wlin dena default security eka ain kra gnn
@@ -19,8 +24,11 @@ public class ProjectSecurityConfig {
 //                .requestMatchers("/api/v1/account/my-account").hasAuthority("admin") me krmeta krot databse eke data saave krnn oni admin,user this type
 //                .requestMatchers("/api/v1/longs/my-long").hasAuthority("user")
                 //below method we have to sava data in data base ROLE_ADMIN,ROLE_USER this type
-                .requestMatchers("/api/v1/account/my-account").hasRole("ADMIN")
-                .requestMatchers("/api/v1/longs/my-long").hasRole("USER")
+//                .requestMatchers("/api/v1/account/my-account").hasRole("ADMIN")
+//                .requestMatchers("/api/v1/longs/my-long").hasRole("USER")
+                //below method have to add role my account end point and long. go to loging controller and acc controller EX=  @Secured("ROLE_ADMIN")
+                .requestMatchers("/api/v1/account/my-account").authenticated()
+                .requestMatchers("/api/v1/longs/my-long").authenticated()
                 .requestMatchers("/api/v1/notices/my-notice","/api/v1/user/register").permitAll()
                 .and().formLogin().and().httpBasic();
         return http.build();
--- src/main/java/springbootacademy/security/controller/AccountController.java
+++ src/main/java/springbootacademy/security/controller/AccountController.java
@@ -1,9 +1,13 @@
 package springbootacademy.security.controller;
 
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 @RequestMapping("/api/v1/account")
@@ -11,7 +15,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class AccountController {
 
     @GetMapping("/my-account")
+    //@Secured("ROLE_ADMIN")
+    @PostAuthorize("hasAuthority('ROLE_ADMIN')")
     public String getAccountDetails(){
+        System.out.println("working");
         return "account controller";
     }
 }
